18567a1e6e7e2a3fc960f6d9cbd35a1efadcc1dc
zkhan907557-oss <zkhan907557@gmail.com>
2025-11-04 04:47:14 -0800

    Add files via upload

---
+++ h
@@ -0,0 +1,7 @@
+marks = 87
+if marks >= 98
+print ( 'you can go to trip')
+else marks  >= 80 and marks <90:
+print ("you will get a new book")
+else marks 70 and marks 80:
+print ("you not get your phone" )
---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from "tailwindcss"
+
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: { extend: {} },
+  plugins: [require("@tailwindcss/typography")]
+} satisfies Config
